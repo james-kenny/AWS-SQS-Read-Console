73bcaab8d6efc7b4d09b97111b18d092592fb579
James Kenny <james.m.kenny@gmail.com>
2016-11-09 16:18:20 +0000

    Add project files.

---
+++ SQS_Read_Message_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQS_Read_Message_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
